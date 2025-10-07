d5ccaaf34b5d699c86c0897473ea4a3a1ba32b5c
abhilash-pinapaka <sarath2097@gmail.com>
2025-10-07 21:06:12 +0530

    Update printer.py
    Signed-off-by: abhilash-pinapaka <sarath2097@gmail.com>

--- onnx/printer.py
+++ onnx/printer.py
@@ -4,7 +4,7 @@
 from __future__ import annotations
 
 import onnx
-import onnx.onnx_cpp2py_export.printer as C  # noqa: N812
+from onnx.onnx_cpp2py_export import printer as C  # noqa: N812
 
 
 def to_text(proto: onnx.ModelProto | onnx.FunctionProto | onnx.GraphProto) -> str:
